cdc2c5011c4c11cc22164304b781128f2d7903d9
Rutik Gagare <rutikgagare091971@gmail.com>
2022-05-30 21:38:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// for hamburger
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline'},350);
+        document.querySelector('.ham').style.display='none';
+    }
+})
+
+// light and dark mode
+var z = document.getElementById("btn");
+var p = document.getElementById("change_mode");
+
+
+// preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display = 'none';
+})
+
+// auto typing
+var typed = new Typed(".auto",{
+    strings:["Developer","Programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
+
+
+
+
+
+
+
+
