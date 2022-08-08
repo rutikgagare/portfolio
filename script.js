773d5a574d8a12abdbc43e412d9c8aa2900e7017
Rutik Gagare <rutikgagare091971@gmail.com>
2022-08-08 23:42:59 +0530

    final

--- script.js
+++ script.js
@@ -14,24 +14,12 @@ document.querySelector('.hamburger').addEventListener("click",()=>{
     }
 })
 
-document.getElementsByClassName('nav').addEventListener('click',()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebargo');
-    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
-        document.querySelector('.ham').style.display='inline';
-        document.querySelector('.cross').style.display='none';
-    }
-    else{
-        setTimeout(()=>{
-        document.querySelector('.cross').style.display='inline'},350);
-        document.querySelector('.ham').style.display='none';
-    }
-})
 
 // light and dark mode
 var z = document.getElementById("btn");
 var p = document.getElementById("change_mode");
 
-preloader
+// preloader
 var loader = document.getElementById("preloader");
 window.addEventListener("load",function(){
     loader.style.display = 'none';
@@ -43,6 +31,19 @@ var typed = new Typed(".auto",{
     typeSpeed:100,
     backSpeed:100,
     loop:true
+});
+
+document.getElementsByClassName('nav').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline'},350);
+        document.querySelector('.ham').style.display='none';
+    }
 });
 
 
