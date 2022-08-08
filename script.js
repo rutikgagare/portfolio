9e5c69165acfbaeceb8f32dfb9d7c570a0195e8c
Rutik Gagare <rutikgagare091971@gmail.com>
2022-08-08 22:43:23 +0530

    changes nav click script

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ document.querySelector('.hamburger').addEventListener("click",()=>{
     }
 })
 
+document.getElementsByClassName('nav').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline'},350);
+        document.querySelector('.ham').style.display='none';
+    }
+})
+
 // light and dark mode
 var z = document.getElementById("btn");
 var p = document.getElementById("change_mode");
